5717fed6a1b5a39902a5ee68a539c202f7ed64d8
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-05-18 13:58:29 -0700

    Initial Commit

---
+++ SwiftUIApiCalls/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIApiCalls
+//
+//  Created by Mehdi MMS on 18/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIApiCalls/SwiftUIApiCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApiCallsApp.swift
+//  SwiftUIApiCalls
+//
+//  Created by Mehdi MMS on 18/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApiCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

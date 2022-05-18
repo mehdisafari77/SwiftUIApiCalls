d70996aab04ba6f4e8ddb33fe04d09898f3c8ab2
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-05-18 14:17:14 -0700

    partly done

--- SwiftUIApiCalls/ContentView.swift
+++ SwiftUIApiCalls/ContentView.swift
@@ -7,10 +7,46 @@
 
 import SwiftUI
 
+struct Course: Hashable, Codable {
+    let name: String
+    let image: String
+}
+
+class ViewModel: ObservableObject {
+    @Published var courses: [Course] = []
+    
+    func fetch() {
+        guard let url = URL(string: "https://iosacademy.io/api/v1/courses/index.php") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { [ weak self ] data, _,
+        error in
+            guard let data = data, error == nil else {
+                return
+            }
+            // Convert to JSON
+            do {
+                let courses = try JSONDecoder().decode([Course].self, from: data)
+                DispatchQueue.main.async {
+                    self?.courses = courses
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                
+            }
+            .navigationTitle("Courses")
+        }
     }
 }
 
